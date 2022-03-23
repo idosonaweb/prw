505805d0a41a28b08a97e037ff6153fae242be0c
Joao <jvieiraluzpiva@gmail.com>
2022-03-23 10:51:03 -0300

    commit

---
+++ Aula5/Ex2.php
@@ -0,0 +1,18 @@
+<?php
+
+    $Nota1 = 7 ;
+
+    $Nota2 = 8 ;
+
+    $Media = ($Nota1 + $Nota2) / 2 ;
+
+    if($Media >= 7)
+    {
+        echo "Você está aprovado!!" ;
+    }
+    else
+    {
+        echo "Você está reprovado!!" ;
+    }
+
+?>

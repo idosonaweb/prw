79559ec8449b2b9ad312b82692ac0fffbd1c4804
idosonaweb <jvieiraluzpiva@gmail.com>
2022-03-29 20:28:32 -0300

    commit

---
+++ Lista2/Ex2.php
@@ -0,0 +1,97 @@
+<?php
+
+    $Mes = 4 ;
+
+    switch ($Mes) 
+    {
+        case 1:
+
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Janeiro" ;
+            
+            break;
+        
+        case 2:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Fevereiro" ;
+            break;
+
+        case 3:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Março" ;
+            break;
+        
+        case 4:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Abril" ;
+            break;
+            
+        case 5:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Maio" ;
+            break;
+    
+        case 6:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Junho" ;
+            break;
+
+        case 7:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Julho" ;
+            break;
+                
+        case 8:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Agosto" ;
+            break;
+        
+        case 9:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Setembro" ;
+            break;
+
+        case 10:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Outubro" ;
+            break;
+                    
+        case 11:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Novembro" ;
+            break;
+            
+        case 12:
+            
+            echo "Número digitado: ". $Mes ;
+
+            echo "<br><br>Esse número corresponde ao mês de Dezembro" ;
+            break;
+        
+            default:
+            echo "Você digitou um número inválido" ;
+            break;
+    }
+
+?>

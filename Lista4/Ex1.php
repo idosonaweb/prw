62093131bdcff385d577f9c0d96a8943cd9f1828
idosonaweb <jvieiraluzpiva@gmail.com>
2022-04-03 00:28:28 -0300

    commit

---
+++ Lista4/Ex1.php
@@ -0,0 +1,42 @@
+<?php
+    
+    echo "<h1>Exercício 1 - Lista 4</h1>" ;
+
+    $N1 = $_GET["numero1"] ;
+
+    $N2 = $_GET["numero2"] ;
+
+    $Soma = $_GET["numero1"]  + $_GET["numero2"];
+
+    $Subtrair = $_GET["numero1"]  - $_GET["numero2"];
+
+    $Dividir = $_GET["numero1"]  / $_GET["numero2"];
+
+    $Multiplicacao = $_GET["numero1"]  * $_GET["numero2"];
+
+    if (isset($_GET["numero1"]) and isset($_GET["numero1"])) 
+    {
+        if ($_GET["optOperacao"] == "Somar") 
+        {
+            echo $N1 . " + " . $N2 . " = " . $Soma ;
+        }
+        else
+        {
+            if ($_GET["optOperacao"] == "Subtrair") 
+            {
+                echo $N1 . " - " . $N2 . " = " . $Subtrair ;
+            }
+            else 
+            {
+                if ($_GET["optOperacao"] == "Dividir") 
+                {
+                    echo $N1 . " / " . $N2 . " = " . $Dividir ;
+                }
+                else
+                {
+                    echo $N1 . " * " . $N2 . " = " . $Multiplicacao ;
+                }
+            }
+        }   
+    }
+?>

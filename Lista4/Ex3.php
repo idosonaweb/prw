2d44dd199c5a9cbff33777c492e628a574d8c6bd
idosonaweb <jvieiraluzpiva@gmail.com>
2022-04-04 09:37:14 -0300

    commit

---
+++ Lista4/Ex3.php
@@ -0,0 +1,110 @@
+<?php
+    
+    echo "<h1>Exercício 3 - Lista 4</h1>" ;
+
+    if (isset($_GET["estado"])) 
+    {
+        if ($_GET["estado"] == "São Paulo") 
+        {
+            echo "A capital de São Paulo é São Paulo" ;
+        }
+        else
+        {
+            if ($_GET["estado"] == "Minas Gerais") 
+            {
+                echo "A capital de Minas Gerais é Belo Horizonte" ;
+            }
+            else 
+            {
+                if ($_GET["estado"] == "Rio de Janeiro") 
+                {
+                    echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                }
+                else
+                {
+                    if ($_GET["estado"] == "Rio de Janeiro") 
+                    {
+                        echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                    }
+                    else
+                    {
+                        if($_GET["estado"] == "Rio de Janeiro")
+                        {
+                            echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                        }
+                        else
+                        {
+                            if($_GET["estado"] == "Rio de Janeiro")
+                            {
+                                echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                            }
+                            else
+                            {
+                                if ($_GET["estado"] == "Rio de Janeiro") 
+                                {
+                                    echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                }
+                                else
+                                {
+                                    if ($_GET["estado"] == "Rio de Janeiro") 
+                                    {
+                                        echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                    }
+                                    else
+                                    {
+                                        if ($_GET["estado"] == "Rio de Janeiro") 
+                                        {
+                                            echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                        }
+                                        else
+                                        {
+                                            if ($_GET["estado"] == "Rio de Janeiro") 
+                                            {
+                                                echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                            }
+                                            else
+                                            {
+                                                if($_GET["estado"] == "Rio de Janeiro")
+                                                {
+                                                    echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                                }
+                                                else
+                                                {
+                                                    if($_GET["estado"] == "Rio de Janeiro")
+                                                    {
+                                                        echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                                    }
+                                                    else
+                                                    {
+                                                        if ($_GET["estado"] == "Rio de Janeiro") 
+                                                        {
+                                                            echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;    
+                                                        }
+                                                        else
+                                                        {
+                                                            if ($_GET["estado"] == "Rio de Janeiro") 
+                                                            {
+                                                                echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                                            }
+                                                            else
+                                                            {
+                                                                if ($_GET["estado"] == "Rio de Janeiro") 
+                                                                {
+                                                                    echo "A capital do Rio de Janeiro é o Rio de Janeiro" ;
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }   
+    }
+?>

be485c12314fcdb0b28d8b7b5799fdf44c7c83bb
idosonaweb <jvieiraluzpiva@gmail.com>
2022-05-18 11:34:16 -0300

    commit

--- Lista6/altera_agenda_exe.php
+++ Lista6/altera_agenda_exe.php
@@ -2,23 +2,53 @@
 
     include('conexao.php');
 
-    $id_usuario = $_GET["id_usuario"];
+    $id_agenda = $_GET["id_agenda"];
 
-    $nome = $_GET["nome_usuario"];
+    $Nome = $_GET["nome_agenda"];
 
-    $email = $_GET["email_usuario"];
+    $Apelido = $_GET["apelido_agenda"];
 
-    $telefone = $_GET["telefone_usuario"];
+    $Endereco = $_GET["nome_agenda"];
+
+    $Bairro = $_GET["nome_agenda"];
+
+    $Cidade = $_GET["nome_agenda"];
+
+    $Estado = $_GET["email_usuario"];
+
+    $Fone = $_GET["telefone_agenda"];
+
+    $Celular = $_GET["celular_agenda"];
+
+    $Email = $_GET["email_agenda"];
+
+    $Data = date('d/m/Y');
 
     echo "<h1>Alteração de dados</h1>" ;
     
-    echo "<p>Nome usuário: " . $nome . "</p>" ;
+    echo "<p>Nome usuário: " . $Nome . "</p>" ;
 
-    $sql = "UPDATE usuario SET
-            nome_usuario ='". $nome ."',
-            email_usuario ='". $email ."',
-            telefone_usuario ='". $telefone . "'
-            WHERE id_usuario =". $id_usuario ;
+    $sql = "UPDATE agenda SET
+            
+            nome_agenda ='". $Nome ."',
+            
+            apelido_agenda ='" . $Apelido . "',
+            
+            endereco_agenda ='" . $Endereco . "',
+            
+            bairro_agenda ='" . $Bairro . "',
+            
+            cidade_agenda ='" . $Cidade . "',
+            
+            estado_agenda ='" . $Estado . "',
+            
+            telefone_agenda ='". $Fone . "',
+            
+            celular_agenda ='" . $Celular . "',
+            
+            email_agenda ='". $Email ."'
+            
+            WHERE id_agenda =". $id_agenda ;
 
     $result = mysqli_query($con, $sql);
 
